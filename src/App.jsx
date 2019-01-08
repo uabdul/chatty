245ea8a2dd62ecc6bca9f2acaea7c99ddfb94ed4
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-01-08 01:54:18 +0000

    Setting up project and putting HTML into JSX files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Message />
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default MessageList;
